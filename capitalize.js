95d3f6b4ada313c818136e0873bac54b104e40f9
Smaddermand <50303469+Smaddermand@users.noreply.github.com>
2024-05-24 06:20:24 +0200

    create capitalize function + test

---
+++ capitalize.js
@@ -0,0 +1,10 @@
+// capitalize.js
+
+function capitalize(string) {
+    
+    const newString = string.charAt(0).toUpperCase() + string.slice(1);
+
+    return newString
+}
+
+module.exports = capitalize;
