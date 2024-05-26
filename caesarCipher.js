f4a15a8d88e21cc8692399610a2973fceca55aad
Smaddermand <50303469+Smaddermand@users.noreply.github.com>
2024-05-26 20:47:49 +0200

    create caeserCiper function + tests

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,10 +1,25 @@
 // ceasarCipher.js
 
-function ceasarCipher(string) {
+function caesarCipher(message, shift) {
+    let result = '';
+    
+    for (let i = 0; i < message.length; i++) {
+      let char = message[i];
+      
+      if (char.match(/[a-z]/i)) {
+        const code = message.charCodeAt(i);
+        
+        if (code >= 65 && code <= 90) {
+          char = String.fromCharCode(((code - 65 + shift) % 26) + 65);
+        } else if (code >= 97 && code <= 122) {
+          char = String.fromCharCode(((code - 97 + shift) % 26) + 97);
+        }
+      }
+      
+      result += char;
+    }
+    
+    return result;
+  }
 
-    const ceasarCipherString = "kl"
-
-    return ceasarCipherString;
-}
-
-module.exports = ceasarCipher;
+module.exports = caesarCipher;
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -1,7 +1,27 @@
-// ceasarCipher.test.js
+// caesarCipher.test.js
 
-const ceasarCipher = require("./caesarCipher")
+const caesarCipher = require("./caesarCipher")
 
 test("expect hi to return kl", () => {
-    expect(ceasarCipher("hi")).toBe("kl");
+    expect(caesarCipher("hi", 3)).toBe("kl");
+})
+
+test("expect hello to return olleh", () => {
+    expect(caesarCipher("hello", 3)).toBe("khoor");
+})
+
+test("expect hi to be ij test shift 1 ", () => {
+    expect(caesarCipher("hi", 1)).toBe("ij");
+})
+
+test("test capitalization", () => {
+    expect(caesarCipher("Hi", 3)).toBe("Kl");
+})
+
+test("punctuation", () => {
+    expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
+})
+
+test("wrapping", () => {
+    expect(caesarCipher("xyz", 3)).toBe("abc");
 })
