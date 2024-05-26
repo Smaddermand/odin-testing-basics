d01a82d166854495d236620a2b234d34fc8cf5c0
Smaddermand <50303469+Smaddermand@users.noreply.github.com>
2024-05-26 21:27:19 +0200

    create max + lenght + tests

--- analyzeArray.js
+++ analyzeArray.js
@@ -1,20 +1,42 @@
 // analyzeArray.js
 
-function analyzeArray(array){
-    object = {
+function analyzeArray(array) {
+    let object = {
         average: 0,
-        min: 1,
-    }
+        min: 0,
+        max: 0,
+        length: 0,
+    };
 
+    // Calculate sum and average
     let sum = 0;
-    for (i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length; i++) {
         sum += array[i];
     }
-    object.average = sum / array.length
+    object.average = sum / array.length;
 
+    // Calculate min
+    object.min = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < object.min) {
+            object.min = array[i];
+        }
+    }
 
+    // Calculate max
+    object.max = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > object.max) {
+            object.max = array[i];
+        }
+    }
+
+    // Calculate length
+    object.length = array.length
 
     return object;
 }
 
 module.exports = analyzeArray;
+
+
--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -11,3 +11,24 @@ test("return array min", () => {
     const data = [1, 8, 3, 4, 2, 6]
     expect(analyzeArray(data).min).toEqual(1);
 });
+
+test("return array min 1 at the end", () => {
+    const data = [6, 8, 3, 4, 2, 6, 1]
+    expect(analyzeArray(data).min).toEqual(1);
+});
+
+test("return array max", () => {
+    const data = [1, 8, 3, 4, 2, 6];
+    expect(analyzeArray(data).max).toBe(8);
+});
+
+test("return array max, largest first", () => {
+    const data = [8, 1, 3, 4, 2, 6]
+    expect(analyzeArray(data).max).toEqual(8);
+});
+
+test("return array length", () => {
+    const data = [8, 1, 3, 4, 2, 6]
+    expect(analyzeArray(data).length).toEqual(6);
+});
+
